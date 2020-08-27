d43e1fcce58b55b180cf88a22b628ba597b63901
Nishat-Tasnim1 <67514902+Nishat-Tasnim1@users.noreply.github.com>
2020-08-28 00:47:23 +0600

    done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            &copy; DN S Dhrubo | 2020
+            &copy; NISHAT TASNIM | 2020
         </div>
     );
 };
